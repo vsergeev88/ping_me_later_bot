3a70895608b471952b64c385c17b9fba7dbbc8cf
Valentin Sergeev <valentin@visme.com>
2024-11-17 23:03:03 +0300

    require

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import TelegramBot from 'node-telegram-bot-api';
+const TelegramBot = require('node-telegram-bot-api');
 import * as dotenv from 'dotenv';
 import { checkReminders } from './utils/checkReminders';
 import { botListeners } from './botListeners';
